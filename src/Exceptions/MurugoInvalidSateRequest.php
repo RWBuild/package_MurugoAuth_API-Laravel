30ab1cc08a57e2faaf0340dab67e66d72549e83d
UweVanOpfern <uweaime@gmail>
2020-06-26 08:58:48 +0200

    feat: new structure of authenticating using passport apporach

---
+++ src/Exceptions/MurugoInvalidSateRequest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace RwandaBuild\MurugoAuth\Exceptions;
+
+use Exception;
+use App\Services\MurugoAuth;
+
+class MurugoInvalidSateRequest extends Exception
+{
+    public function __construct($message = '')
+    {
+        parent::__construct($message);
+        $this->status = 403;
+    }
+
+    /**
+     * Laravel property
+     * This will be called by the framework
+     */
+    public function render()
+    {
+        $resp = $this->buildResponse();
+
+        return $resp;
+    }
+
+    /**
+     * Response to return for this exception
+     */
+    public function buildResponse()
+    {
+        return $this->detectResponseType();
+    }
+
+    /**
+     * reponse for web and ip request
+     */
+    private function detectResponseType()
+    {
+        if (! request()->expectsJson()) {
+            session()->flash('status-error', $this->getMessage());
+            return MurugoAuth::redirect();
+        }
+
+        return response()->json($this->buildData(), $this->status);
+    }
+
+    /**
+     * arrange the response message
+     */
+    public function buildData()
+    {
+        return [
+            'success' => false,
+            'message' => $this->getMessage() 
+        ];
+    }
+}
--- src/Facades/MurugoAuth2.php
+++ src/Facades/MurugoAuth2.php
@@ -8,8 +8,13 @@
 
 namespace RwandaBuild\MurugoAuth\Facades;
 
+use Illuminate\Support\Facades\Facade;
 
-class MurugoAuth2
+
+class MurugoAuth2 extends Facade
 {
-
+    protected static function getFacadeAccessor()
+    {
+        return 'MurugoAuthHandler';
+    }
 }
